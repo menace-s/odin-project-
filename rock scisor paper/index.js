86d145c7295a70eea1f997076aec6fc3aea088d9
aganh kola jean philippe <akolajeanphilippe@gmail.com>
2024-03-10 05:53:27 +0000

    version 3 effectue tout le travail

--- rock scisor paper/index.js
+++ rock scisor paper/index.js
@@ -1,5 +1,7 @@
 const table = ["rock", "paper", "scisor"];
 const button = document.querySelectorAll(".btn");
+const displayScore=document.querySelector(".displayScore");
+const displayVictory=document.querySelector(".displayVictory");
 let computerChoice;
 let userChoice;
 let scoreUser = 0,
@@ -13,18 +15,21 @@ function getComputerChoice() {
 }
 
 function determineWinner(scoreUser, scoreComputer) {
-  if (scoreUser > 0 && scoreUser%5 == 0) {
+  if (scoreUser == 5) {
+    displayVictory.textContent='Victory'
     return "Victory";
-  } else if (scoreComputer > 0 && scoreComputer%5 == 0) {
+  } else if (scoreComputer == 5) {
+    displayVictory.textContent='Failure'
     return "Failure";
   }
 }
 
 // function which consists in making a round
 function playRound(userChoice, computerChoice) {
+  displayVictory.textContent=""
   userChoice = userChoice.toLowerCase();
   if (userChoice === computerChoice) {
-    
+    console.log("tie")
   } else if (
     (userChoice === "rock" && computerChoice === "scisor") ||
     (userChoice === "paper" && computerChoice === "rock") ||
@@ -40,6 +45,7 @@ function playRound(userChoice, computerChoice) {
     console.log(scoreUser + " " + scoreComputer);
     // return "failure";
   }
+  displayScore.textContent=`${scoreUser} - ${scoreComputer}`
 }
 
 
@@ -52,37 +58,3 @@ button[1].addEventListener("click", () => {
 button[2].addEventListener("click", () => {
   playRound("paper", getComputerChoice());
 });
-// function that consists in going 5 rounds and recovering the score
-// function playGame() {
-//   // use of for for all 5 rounds
-//   for (let i = 0; i < 5; i++) {
-//     let userChoice = prompt();
-//     let result;
-//     result = playRound(userChoice, getComputerChoice());
-//     switch (result) {
-//       case "victory":
-//         console.log("round won");
-//         scoreUser += 1;
-//         break;
-//       case "failure":
-//         console.log("round failure");
-//         scoreComputer += 1;
-//         break;
-//       case "Equality !":
-//         console.log("Equality !");
-//     }
-//   }
-//   // comparison of score values
-//   console.log(scoreUser);
-//   if (scoreUser > scoreComputer) {
-//     console.log(`you won your score is ${scoreUser} over 5 rounds`);
-//   } else if (scoreComputer > scoreUser) {
-//     console.log(`vos avez perdu votre score est ${scoreUser} sur 5 round`);
-//   } else {
-//     console.log(
-//       `Tie! Your score is ${scoreUser} and the computer's is ${scoreComputer}.`
-//     );
-//   }
-// }
-
-// playGame();
