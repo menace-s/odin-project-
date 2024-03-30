ae82cd9b2ee64c3b7fa6ad0b9dbc9b325b6b50dd
aganh kola jean philippe <akolajeanphilippe@gmail.com>
2024-03-30 12:22:22 +0000

    version 2 reste a ajouter button pour supprimer

--- library app/script.js
+++ library app/script.js
@@ -1,6 +1,7 @@
 // Définition d'un tableau pour stocker les livres
 const myLibrary = [];
 
+submitBtn=document.getElementById("submitBtn").addEventListener("click",addBookToLibrary)
 // Définition du constructeur de l'objet "book"
 function book(title, author, pages, read) {
     this.title = title;         // Titre du livre
@@ -9,8 +10,33 @@ function book(title, author, pages, read) {
     this.read = read;           // Indicateur si le livre a été lu ou non
 }
 
+// Fonction pour afficher les éléments de la bibliothèque dans un tableau HTML
+function displayElmtOfTable(table) {
+    // Récupération de l'élément HTML où afficher le tableau
+    displayTab = document.getElementById("displayTab");
+
+    // Parcours de chaque livre dans la bibliothèque
+    for (let i = 0; i < table.length; i++) {
+        // Création d'une nouvelle ligne dans le tableau
+        tabNewRow = document.createElement("tr");
+        displayTab.appendChild(tabNewRow);
+
+        // Parcours de chaque propriété du livre
+        for (let element in table[i]) {
+            // Création d'une nouvelle colonne dans le tableau
+            tabNewColumn = document.createElement("td");
+            // Ajout du contenu de la propriété à la colonne
+            tabNewColumn.textContent = table[i][element];
+            tabNewRow.appendChild(tabNewColumn);
+        }
+    }
+    //vider le tableau pour ne pas avoir les elements double lors de l'affichage
+    myLibrary.splice(0, myLibrary.length)
+}
 // Fonction pour ajouter un livre à la bibliothèque
-function addBookToLibrary() {
+function addBookToLibrary(event) {
+    //annuler le chargement de la page
+    event.preventDefault();
     // Récupération des valeurs des champs du formulaire
     const bookTitleInput = document.getElementById("bookTitle").value;
     const bookAuthorInput = document.getElementById("bookAuthor").value;
@@ -36,27 +62,12 @@ function addBookToLibrary() {
     console.log(myLibrary);
 }
 
-// Fonction pour afficher les éléments de la bibliothèque dans un tableau HTML
-function displayElmtOfTable(table) {
-    // Récupération de l'élément HTML où afficher le tableau
-    displayTab = document.getElementById("displayTab");
-
-    // Parcours de chaque livre dans la bibliothèque
-    for (let i = 0; i < table.length; i++) {
-        // Création d'une nouvelle ligne dans le tableau
-        tabNewRow = document.createElement("tr");
-        displayTab.appendChild(tabNewRow);
-
-        // Parcours de chaque propriété du livre
-        for (let element in table[i]) {
-            // Création d'une nouvelle colonne dans le tableau
-            tabNewColumn = document.createElement("td");
-            // Ajout du contenu de la propriété à la colonne
-            tabNewColumn.textContent = table[i][element];
-            tabNewRow.appendChild(tabNewColumn);
-        }
-    }
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
 }
 
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
 // console.log(table)
 // displayElmtOfTable(table);
